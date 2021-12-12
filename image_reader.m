de83951ace9d786b1886cecd065910ad9a648ae6
Tryanno5 <77161110+Tryanno5@users.noreply.github.com>
2021-12-12 10:33:35 -0400

    Add files via upload

---
+++ image_reader.m
@@ -0,0 +1,39 @@
+clear;
+clc;
+
+enteryNumber = 162;
+
+data = readtable('C:\Users\RoyMc\Documents\Programing\IdeaProjects\neural net test\train.csv');
+data = data{:, :};
+img = 255 - imread("C:\Users\RoyMc\Documents\Programing\number ttest.png");
+
+temp = zeros(28);
+temp2 = zeros(28);
+
+for i = 1:28
+    for j = 1:28
+        temp(j,i) = data(enteryNumber, i+(j-1)*28 );
+    end
+end
+
+for i = 1:28
+    for j = 1:28
+        temp2(i,j) = mean(img(i,j));
+    end
+end
+
+index = 1;
+clf;
+hold on;
+
+for i = 1:28
+    for j = 1:28
+        c = (255-temp(i,j))/255;
+        color = [c, c, c];
+        plot(j, 29-i, "s", "Color", color, 'MarkerSize',10,'MarkerFaceColor',color)
+        index = index + 1;
+    end
+end
+
+axis([0 29 0 29])
+
